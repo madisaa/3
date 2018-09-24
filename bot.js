32a8ad766a4b1ad86743773aac74c6a68f7d343a
madisaa <43490282+madisaa@users.noreply.github.com>
2018-09-24 23:41:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1014,17 +1014,4 @@ client.on ("guildMemberRemove", member => {
 
 })
 
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
 client.login(process.env.BOT_TOKEN);
